692ceaba3ea214256ac6cb0a25c21659066c433c
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-04-23 16:43:01 +0600

    Add news.json

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.use(cors())
 
@@ -12,6 +13,27 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id
+    if (id === '0') {
+        res.send(news)
+    }
+    else {
+        const selectedCategory = news.filter(n => n.category_id === id)
+        res.send(selectedCategory)
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
 })
 
 app.listen(port, () => {
